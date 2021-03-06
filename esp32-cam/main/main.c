7c5e4367bcca7a20ff5e8f6c67b4add38509a40e
Zack <zsielck@purdue.edu>
2021-03-06 14:53:01 -0500

    This is more convoluted than necessary

--- esp32-cam/main/main.c
+++ esp32-cam/main/main.c
@@ -9,6 +9,7 @@
 #include "freertos/task.h"
 #include "freertos/event_groups.h"
 #include "freertos/semphr.h"
+#include "esp_task_wdt.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
@@ -27,8 +28,13 @@
 
 #include "perfmon.h"
 
-#define TOTAL_CALL_AMOUNT 200
+//#define CONFIG_FREERTOS_HZ 1000
+//#define CONFIG_ESP_INT_WDT 10000
+#define TOTAL_CALL_AMOUNT 200 // Used with Performance Monitor
 #define PERFMON_TRACELEVEL -1 // -1 - will ignore trace level
+#define DEV_DEBUG_PRESENT
+#define MOTOR1 16
+#define MOTOR2 17
 
 // Function Declarations //
 
@@ -64,8 +70,6 @@ void app_main(void)
       ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret); */
-    SemaphoreHandle_t speedMutex;
-    float speed = 0;
     printf("Hello Zack!\n");
     
     //learning_led();
@@ -76,14 +80,9 @@ void app_main(void)
 
     mutexed_float_t * motor_memory = setup_motor_encoders();
     //perfMon((void *)setup_motor_encoders);
-    speedMutex = (SemaphoreHandle_t) (((SemaphoreHandle_t *)motor_memory->mutexSemaphores)[3]);
     for(;;){
-        if(xSemaphoreTake(speedMutex, 100/portTICK_PERIOD_MS)){
-            speed = motor_memory->mem[3];
-            xSemaphoreGive(speedMutex);
-            printf("Speed = %f", speed);
-            vTaskDelay(3000/ portTICK_PERIOD_MS); // 3000 ms delay
-        }
+        vTaskDelay(3000/ portTICK_PERIOD_MS); // 3000 ms delay
+        printf("Speed = %f\n", motor_memory->mem[2]);
     }
 }
 
@@ -103,108 +102,198 @@ void perfMon(void * called_function){
 }
 
 void update_motor_encoder1(void * params){
-    mutexed_float_t * m = (mutexed_float_t * ) params;
-    SemaphoreHandle_t tempSem = (SemaphoreHandle_t) (((SemaphoreHandle_t *)m->mutexSemaphores)[0]);
-    if(xSemaphoreTake(tempSem, 100/portTICK_PERIOD_MS)){
+    static mutexed_float_t * m = 0x0;
+    static pthread_mutex_t tempSem = 0x0;
+    static esp_err_t err = ESP_OK;
+    m = (mutexed_float_t * ) params;
+    
+    tempSem = (pthread_mutex_t) (((pthread_mutex_t *)m->mutexSemaphores)[0]);
+    err = pthread_mutex_trylock(&tempSem);
+    if(err != ESP_OK){
+        printf("Interupt collision. We need this to work.\n");
+    }else{
         m->mem[0] = m->mem[0] + 374;
-        xSemaphoreGive(tempSem);
+        pthread_mutex_unlock(&tempSem);
+        printf("Ticks2: %f\n", m->mem[0]);
     }
 }
 
 void update_motor_encoder2(void * params){
-    mutexed_float_t * m = (mutexed_float_t * ) params;
-    SemaphoreHandle_t tempSem = (SemaphoreHandle_t) (((SemaphoreHandle_t *)m->mutexSemaphores)[1]);
-    if(xSemaphoreTake(tempSem, 100/portTICK_PERIOD_MS)){
+    static mutexed_float_t * m = 0x0;
+    static pthread_mutex_t tempSem = 0x0;
+    static esp_err_t err = ESP_OK;
+    m = (mutexed_float_t * ) params;
+
+    tempSem = (pthread_mutex_t) (((pthread_mutex_t *)m->mutexSemaphores)[1]);
+    err = pthread_mutex_trylock(&tempSem);
+    if(err != ESP_OK){
+        printf("Interupt collision. We need this to work.\n");
+    }else{
         m->mem[1] = m->mem[1] + 374;
-        xSemaphoreGive(tempSem);
+        pthread_mutex_unlock(&tempSem);
+        printf("Ticks2: %f\n", m->mem[1]);
     }
 }
 
 void measure_speed_calc(void * params){
-    mutexed_float_t * m = (mutexed_float_t * ) params;
-    float motor1 = 1000;
-    float motor2 = 1000;
-    uint64_t time_dif = 1;
-    SemaphoreHandle_t tempSem;
-    struct timeval tv_now;
-    uint64_t time_us = 0;
+    static mutexed_float_t * m = 0x0;
+    static float motor1 = 1000;
+    static float motor2 = 1000;
+    static uint64_t time_dif = 1;
+    static pthread_mutex_t tempSem = 0x0;
+    static struct timeval tv_now;
+    static esp_err_t err = ESP_OK;
+    static int count = 0;
 
+    m = (mutexed_float_t * ) params;
 
     for(;;){
+        printf("Calculating Speed...\n\n");
+        // Execute before managing memory locks
         gettimeofday(&tv_now, NULL);
-        time_us = (uint64_t)tv_now.tv_sec * 1000000L + (uint64_t)tv_now.tv_usec;
-
-        tempSem = (SemaphoreHandle_t) (((SemaphoreHandle_t *)m->mutexSemaphores)[0]);
-        if(xSemaphoreTake( tempSem, 100/portTICK_PERIOD_MS)){
-            motor1 = m->mem[0];
-            m->mem[0] = 1000;
-            xSemaphoreGive(tempSem);
-            tempSem = (SemaphoreHandle_t) (((SemaphoreHandle_t *)m->mutexSemaphores)[1]);
-            if(xSemaphoreTake(tempSem, 100/portTICK_PERIOD_MS)){
-                motor2 = m->mem[1];
-                m->mem[1] = 1000;
-                xSemaphoreGive(tempSem);
-                tempSem = (SemaphoreHandle_t) (((SemaphoreHandle_t *)m->mutexSemaphores)[2]);
-                if(xSemaphoreTake(tempSem, 100/portTICK_PERIOD_MS)){
-                    time_dif = time_us - m->mem[2];
-                    gettimeofday(&tv_now, NULL);
-                    m->mem[2] = (uint64_t)tv_now.tv_sec * 1000000L + (uint64_t)tv_now.tv_usec;
-                    xSemaphoreGive(tempSem);
-                    tempSem = (SemaphoreHandle_t) (((SemaphoreHandle_t *)m->mutexSemaphores)[3]);
-                    if(xSemaphoreTake(tempSem, 100/portTICK_PERIOD_MS)){
-                        // Hall feedback is 374-> 34:1 gear ratio with 11 ticks per round (11*34=374)
-                        m->mem[3] = ((motor1 + motor2) + 2) / time_dif / 748;
-                        xSemaphoreGive(tempSem);
-                        printf("Speed: %f\n",m->mem[3]);
-                        vTaskDelay(10000/ portTICK_PERIOD_MS);
-                    }
-                }
-            }
+        time_dif = (uint64_t)tv_now.tv_sec * 1000000L + (uint64_t)tv_now.tv_usec - m->mem[3];
+        
+        err = gpio_intr_disable(MOTOR1);
+        if(err != ESP_OK){
+            printf("Handles error %x by discarding the attempt at stopping interrupt.\n",err);
+            continue;
         }
+        err = gpio_intr_disable(MOTOR2);
+        if(err != ESP_OK){
+            printf("Handles error %x by discarding the attempt at stopping interrupt.\n",err);
+            continue;
+        }
+        
+        // | You hold the keys to the memory associated with motor speed calculation here  |  //
+        // |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |    |  //
+        // V    V    V    V    V    V    V    V    V    V    V    V    V    V    V    V    V  //
+        do{
+            // Attempt to grab next mutex locking the memory used to update our speed calculation algorithm
+            // Be careful once you get the value because it needs to be fast so that we dont block the incoming data for long
+            // You have a memory element! Quickly do a couple of operations! //
+            switch(count){
+                case 0:
+                    motor1 = m->mem[count]; //  LD STR
+                    tempSem = (pthread_mutex_t) (((pthread_mutex_t *)m->mutexSemaphores)[count]);
+                    err = pthread_mutex_trylock(&tempSem);
+                    if(err != ESP_OK){
+                        printf("Handles error %x by discarding the attempt at calculating speed.\n",err);
+                        break;
+                    }
+                    
+                    m->mem[count] = 1000; //  ADD 1 STR
+                    
+                    // You have a memory element! Quickly give it back! //
+                    err = pthread_mutex_unlock(&tempSem);
+                    break;
+                case 1:
+                    motor2 = m->mem[count]; // LD STR
+                    tempSem = (pthread_mutex_t) (((pthread_mutex_t *)m->mutexSemaphores)[count]);
+                    err = pthread_mutex_trylock(&tempSem);
+                    if(err != ESP_OK){
+                        printf("Handles error %x by discarding the attempt at calculating speed.\n",err);
+                        break;
+                    }
+
+                    m->mem[count] = 1000; //  ADD 1 STR //  ADD 1 STR
+                    // You have a memory element! Quickly give it back! //
+                    err = pthread_mutex_unlock(&tempSem);
+                    break;
+                case 2:
+                    tempSem = (pthread_mutex_t) (((pthread_mutex_t *)m->mutexSemaphores)[count]);
+                    err = pthread_mutex_trylock(&tempSem);
+                    if(err != ESP_OK){
+                        break;
+                    }
+                     // Hall feedback is 374-> 34:1 gear ratio with 11 ticks per round (11*34=374)
+                    m->mem[count] = ((motor1 + motor2) + 2) / time_dif / 748; // MATHS
+                    
+                    #ifdef DEV_DEBUG_PRESENT 
+                    //printf("Speed: %f\n",m->mem[3]);
+                    #endif
+
+                    err = pthread_mutex_unlock(&tempSem);
+                    break;
+                case 3:
+                    gettimeofday(&tv_now, NULL);
+                    tempSem = (pthread_mutex_t) (((pthread_mutex_t *)m->mutexSemaphores)[count]);
+                    err = pthread_mutex_trylock(&tempSem);
+                    if(err != ESP_OK){
+                        break;
+                    }
+                    
+                    m->mem[count] = (uint64_t)tv_now.tv_sec * 1000000L + (uint64_t)tv_now.tv_usec;
+                    
+                    err = pthread_mutex_unlock(&tempSem);
+                    break;
+            }
+            count += 1;
+        }while(count <= 3);
+
+        err = gpio_intr_enable(MOTOR1);
+        if(err != ESP_OK){
+            printf("Interrupt not scheduled. Err %x.\n",err);
+            break;
+        }
+        
+        err = gpio_intr_enable(MOTOR2);
+        if(err != ESP_OK){
+            printf("Interrupt not scheduled. Err %x.\n",err);
+            break;
+        }
+        // Reset count and wait for a bit before we recalculate
+        count = 0;
+        printf("Next Caclulaion in 5s\n");
+        vTaskDelay(5000/ portTICK_PERIOD_MS);
     }
+    printf("Should not get here\n");
 }
 
 void setup_motor_encoder_pins(mutexed_float_t * mem){
     // Allocate memory for encoder_pin configuration
-    esp_err_t ret = ESP_OK;
+    esp_err_t err = ESP_OK;
     
     // Install the GPIO ISR handler service; allows per-pin interrupt handlers
     gpio_uninstall_isr_service();
-    ESP_ERROR_CHECK(ret);
-    ret = gpio_install_isr_service(ESP_INTR_FLAG_SHARED);
-    ESP_ERROR_CHECK(ret);
-    //ret = gpio_install_isr_service(ESP_INTR_FLAG_SHARED);
+    
+    err = gpio_install_isr_service(ESP_INTR_FLAG_LEVEL1);
+    ESP_ERROR_CHECK(err);
+    //err = gpio_install_isr_service(ESP_INTR_FLAG_SHARED);
+
+    // Initialize pin 14 as an input
+    err = gpio_set_direction(MOTOR1, (gpio_mode_t) 1);
+    ESP_ERROR_CHECK(err);
 
     // Initialize pin 15 as an input
-     // Pin 15
-    ret = gpio_set_direction(14, (gpio_mode_t) 1);
-    ESP_ERROR_CHECK(ret);
+    err = gpio_set_direction(MOTOR2, (gpio_mode_t) 1);
+    ESP_ERROR_CHECK(err);
 
-    ret = gpio_set_direction(15, (gpio_mode_t) 1);
-    ESP_ERROR_CHECK(ret);
+    // Set Interrupt on pin as an input
+    err = gpio_set_intr_type(MOTOR1,GPIO_INTR_POSEDGE);
+    ESP_ERROR_CHECK(err);
 
-    ret = gpio_set_intr_type(14,GPIO_INTR_POSEDGE);
-    ESP_ERROR_CHECK(ret);
-
-    ret = gpio_set_intr_type(15,GPIO_INTR_POSEDGE);
-    ESP_ERROR_CHECK(ret);
+    // Set Interrupt on pin as an input
+    err = gpio_set_intr_type(MOTOR2,GPIO_INTR_POSEDGE);
+    ESP_ERROR_CHECK(err);
     
-    // Add interupt handler to pin16; register the posedge interrupt
-    ret = gpio_isr_handler_add(14, update_motor_encoder1, (void *) mem);
-    ESP_ERROR_CHECK(ret);
+    // Add interupt handler to pin14; register the posedge interrupt
+    err = gpio_isr_handler_add(MOTOR1, update_motor_encoder1, (void *) mem);
+    ESP_ERROR_CHECK(err);
 
-    // Add interupt handler to pin16; register the posedge interrupt
-    ret = gpio_isr_handler_add(15, update_motor_encoder2, (void *) mem);
-    ESP_ERROR_CHECK(ret);
+    // Add interupt handler to pin15; register the posedge interrupt
+    err = gpio_isr_handler_add(MOTOR2, update_motor_encoder2, (void *) mem);
+    ESP_ERROR_CHECK(err);
 
-    // Enable the Rising edge interrupts on pins 15&16; ISRs registered to 'update_motor_encoder' functions
-    gpio_intr_enable(14);
-    ESP_ERROR_CHECK(ret);
+    // Enable the Rising edge interrupts on pin 14
+    err = gpio_intr_enable(MOTOR1);
+    ESP_ERROR_CHECK(err);
 
-    ret = gpio_intr_enable(15);
-    ESP_ERROR_CHECK(ret);
+    // Enable the Rising edge interrupts on pin 15
+    err = gpio_intr_enable(MOTOR2);
+    ESP_ERROR_CHECK(err);
 
-    printf("Configured\n\n");
+    // ISRs activated to 'update_motor_encoder' as ASYNC functions
+    printf("Motor Encoder Interrupts Configured\n\n");
 }
 
 mutexed_float_t * setup_motor_encoders(){
